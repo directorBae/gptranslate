f5a20becfe3b6582cc1d212d0cb9a3b851599bb8
Jeongwon, Bae <bjw020615@gmail.com>
2024-06-16 16:37:15 +0900

    seo

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { hydrate, render } from "react-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-if (root.hasChildNodes()) {
-  hydrate(<App />, root);
-} else {
-  render(<App />, root);
-}
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
